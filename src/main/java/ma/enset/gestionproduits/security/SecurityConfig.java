13b3f00bfc78cc9b492ba55168690a223a78d8ab
dalalyo <younessdalal1@outlook.com>
2025-06-25 15:52:51 +0100

    v3

---
+++ src/main/java/ma/enset/gestionproduits/security/SecurityConfig.java
@@ -0,0 +1,45 @@
+package ma.enset.gestionproduits.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("mohammed").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("othmane").password(passwordEncoder().encode("1234")).roles("USER").build(),
+                User.withUsername("dalal").password(passwordEncoder().encode("1234")).roles("USER","ADMIN").build()
+
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpsecurity) throws Exception {
+
+        return httpsecurity
+                .formLogin(Customizer.withDefaults())
+                .authorizeHttpRequests(aur -> aur.requestMatchers("/user/**").hasRole("USER"))
+                .authorizeHttpRequests(aur -> aur.requestMatchers("/admin/**").hasRole("ADMIN"))
+                .authorizeHttpRequests(aur -> aur.requestMatchers("/public/**").permitAll())
+                .authorizeHttpRequests(a -> a.anyRequest().authenticated())
+                .build();
+    }
+}
--- src/main/java/ma/enset/gestionproduits/web/ProductController.java
+++ src/main/java/ma/enset/gestionproduits/web/ProductController.java
@@ -1,11 +1,15 @@
 package ma.enset.gestionproduits.web;
 
+import jakarta.validation.Valid;
 import ma.enset.gestionproduits.entites.Product;
 import ma.enset.gestionproduits.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +20,7 @@ public class ProductController {
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/index")
+    @GetMapping("/user/index")
     public String index(Model model) {
         List<Product> products = productRepository.findAll();
         model.addAttribute("productList",products );
@@ -24,18 +28,28 @@ public class ProductController {
     }
     @GetMapping("/")
     public String home(Model model) {
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/delete")
+    @GetMapping("/admin/delete")
     public String delete(@RequestParam (name = "id") Long id) {
         productRepository.deleteById(id);
-        return "redirect:/index";
+        return "redirect:/user/index";
     }
 
-    @GetMapping("/newProduct")
+    @GetMapping("/admin/newProduct")
     public String newProduct(Model model) {
         model.addAttribute("product",new Product());
         return "new-product";
+    }
+
+    @PostMapping("/admin/saveProduct")
+    public String saveProduct(@Valid Product product, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "new-product";
+        }
+        productRepository.save(product);
+        return "redirect:/admin/newProduct";
+
     }
 }
